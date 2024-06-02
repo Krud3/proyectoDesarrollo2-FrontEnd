445bfaf2b4daca29dbbcb0b9530569438dc0131c
Estiven <estivenfrv@gmail.com>
2024-06-01 22:13:47 -0500

    Delete comments

--- src/test/login/ErrorMessage.test.jsx
+++ src/test/login/ErrorMessage.test.jsx
@@ -1,20 +1,4 @@
-/*import { describe } from "vitest";
-import ErrorMessage from "./ErrorMessage";
-import { render, screen } from "@testing-library/react";
 
-describe("ErrorMessage", () => {
-    it('renders default error state', () => {
-        render(<ErrorMessage/>);
-        expect(screen.getByTestId('message-container'))
-        .toHaveTextContent('Something went wrong');
-    });
-
-    it('renders default error state', () => {
-        render(<ErrorMessage message="Email is already taken"/>);
-        expect(screen.getByTestId('message-container'))
-        .toHaveTextContent('Email is already taken');
-    });
-});*/
 import { describe, it, expect } from 'vitest';
 
 describe('Test Dummy', () => {
