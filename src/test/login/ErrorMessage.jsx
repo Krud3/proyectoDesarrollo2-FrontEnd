fff22fb1e96339ef54e834b0b65b45650dba817e
Estiven <estivenfrv@gmail.com>
2024-06-01 22:12:59 -0500

    change in the index.css and correction about the grammatic in the errorMessage

--- src/test/login/ErrorMessage.jsx
+++ src/test/login/ErrorMessage.jsx
@@ -1,5 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 const ErrorMessage = ({ message = "Something went wrong" }) => (
     <div data-testid="message-container"> {message} </div>
 );
+
+ErrorMessage.propTypes = {
+    message: PropTypes.string
+};
 
 export default ErrorMessage;
